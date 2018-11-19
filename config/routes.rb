27b3b6bf31b5d240204af2c9192391bf653be806
Gianpaolo D'Amato <damatohg@gmail.com>
2018-11-19 17:15:09 -0400

    RegistrationsController singular names Fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
-    get 'signup', to: 'devise/registrations#new'
+    get 'signup', to: 'registrations#new'
   end
 
   # Root
