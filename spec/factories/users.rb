9c093a1e4cd907f8b6f228941f327c34b3716d31
Gianpaolo D'Amato <damatohg@gmail.com>
2018-11-20 11:22:41 -0400

    Add a users factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:name) { |n| "test#{n}" }
+    sequence(:email) { |n| "test#{n}#test.com" }
+    password { '123456789' }
+    password_confirmation { '123456789' } 
+  end
+end
